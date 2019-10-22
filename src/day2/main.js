472880cdc6b1520afef1a5d183f33f775d33d2c2
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2019-10-22 16:44:33 +0700

    [Edited] - Update Day 2 Coding

--- src/day2/main.js
+++ src/day2/main.js
@@ -1,5 +1,11 @@
 function main(str, str2) {
-
+  let diffCount = 0
+  for (let indexStr = 0, indexStr2 = 0; indexStr < str.length && indexStr2 < str2.length; indexStr++, indexStr2++) {
+    if (str[indexStr] !== str2[indexStr2]) {
+      diffCount++
+    }
+  }
+  return diffCount
 }
 
 module.exports = { main };
