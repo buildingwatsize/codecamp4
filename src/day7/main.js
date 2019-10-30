9fb08947b338bdd8b079a974e453586a739cf7b8
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2019-10-30 09:43:54 +0700

    [Added] - Add Solution of Kata day 7

--- src/day7/main.js
+++ src/day7/main.js
@@ -1,5 +1,14 @@
 function main(arr) {
-	// Your code begins here;
+	// THANKS: https://stackoverflow.com/questions/42773836/how-to-find-all-subsets-of-a-set-in-javascript
+	return arr.reduce(
+		(subsets, value) => subsets.concat(
+			subsets.map(set => [value, ...set].sort())
+		),
+		[[]]
+	);
 }
 
 module.exports = { main };
+
+// console.log(main([1]));
+// console.log(main([1, 2]));
